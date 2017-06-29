09f226fdb0ea952148c3cb087bad93245979732c
Joaquin Caro <me@joaquin-caro.es>
2017-06-29 23:53:23 +0200

    Green: user can not get trips from not friend

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -5,19 +5,38 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
 public class TripServiceShould {
 
     @Test(expected = UserNotLoggedInException.class)
     public void throw_an_exception_if_user_is_not_logged() throws Exception {
-        TripService tripService = new TripServiceTested();
+        TripService tripService = new TripServiceTested(null);
 
         tripService.getTripsByUser(new User());
     }
 
+    @Test
+    public void user_without_friends_can_not_retrive_trips() throws Exception {
+        TripService tripService = new TripServiceTested(new User());
+        List<Trip> trips = tripService.getTripsByUser(new User());
+
+        assertThat(trips.size(),is(0));
+    }
+
     private class TripServiceTested extends TripService {
+        private User user;
+
+        public TripServiceTested(User user) {
+            this.user = user;
+        }
+
         @Override
         protected User getLoggedUser() {
-            return null;
+            return user;
         }
     }
 }
