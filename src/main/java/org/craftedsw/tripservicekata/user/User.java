563fa623d1d274ba080b58b9f1ef5882aa7e39f5
Joaquin Caro <me@joaquin-caro.es>
2017-06-30 00:01:54 +0200

    Green: user can get friends trips

--- src/main/java/org/craftedsw/tripservicekata/user/User.java
+++ src/main/java/org/craftedsw/tripservicekata/user/User.java
@@ -6,9 +6,12 @@ import java.util.List;
 public class User {
 
 	private List<User> friends = new ArrayList<User>();
-	
+
 	public List<User> getFriends() {
 		return friends;
 	}
-	
+
+	public void setFriends(List<User> friends) {
+		this.friends = friends;
+	}
 }
--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -8,35 +8,56 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class TripServiceShould {
 
     @Test(expected = UserNotLoggedInException.class)
     public void throw_an_exception_if_user_is_not_logged() throws Exception {
-        TripService tripService = new TripServiceTested(null);
+        TripService tripService = new TripServiceTested(null, null);
 
         tripService.getTripsByUser(new User());
     }
 
     @Test
     public void user_without_friends_can_not_retrive_trips() throws Exception {
-        TripService tripService = new TripServiceTested(new User());
+        TripService tripService = new TripServiceTested(new User(), null);
         List<Trip> trips = tripService.getTripsByUser(new User());
 
         assertThat(trips.size(),is(0));
     }
 
+    @Test
+    public void user_can_get_friend_trips() throws Exception {
+
+        User me = new User();
+        TripService tripService = new TripServiceTested(me, Arrays.asList(new Trip()));
+        User friend = new User();
+
+        friend.setFriends(Arrays.asList(me));
+        List<Trip> trips = tripService.getTripsByUser(friend);
+
+        assertThat(trips.size(),is(1));
+    }
+
     private class TripServiceTested extends TripService {
         private User user;
+        private List<Trip> trips;
 
-        public TripServiceTested(User user) {
+        public TripServiceTested(User user, List<Trip> trips) {
             this.user = user;
+            this.trips = trips;
         }
 
         @Override
         protected User getLoggedUser() {
             return user;
+        }
+
+        @Override
+        protected List<Trip> getTripsBy(User user) {
+            return trips;
         }
     }
 }
