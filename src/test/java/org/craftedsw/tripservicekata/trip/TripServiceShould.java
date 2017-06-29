d2b3d449b670e94830a0cd924f7f364e5724bd42
Joaquin Caro <me@joaquin-caro.es>
2017-06-29 23:43:39 +0200

    Green: throw exception if user is not logged

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -1,7 +1,23 @@
-package org.craftedsw.tripservicekata;
+package org.craftedsw.tripservicekata.trip;
 
-/**
- * Created by joaco on 29/6/17.
- */
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
 public class TripServiceShould {
+
+    @Test(expected = UserNotLoggedInException.class)
+    public void throw_an_exception_if_user_is_not_logged() throws Exception {
+        TripService tripService = new TripServiceTested();
+
+        tripService.getTripsByUser(new User());
+    }
+
+    private class TripServiceTested extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return null;
+        }
+    }
 }
